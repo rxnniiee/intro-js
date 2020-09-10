8b9dbf57c2573df61fe5c505f7dbd483f7889641
rxnniiee <23708969+rxnniiee@users.noreply.github.com>
2020-09-10 11:03:30 +0300

    wrote main clock functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-var HOURHAND = document.getElementsById('hour');
-var MINUTEHAND = document.querySelector('.minute');
-var SECONDHAND = document.getElementsByName('second');
+const HOURHAND    = document.querySelector('#hour')
+const MINUTEHAND  = document.querySelector('#minute')
+const SECONDHAND  = document.querySelector('#second')
+
+// clock's functionality
+function tick() {
+  const date = new Date()
+
+  const deltaHours    = (360 / 60) * date.getHours()
+  const deltaMinutes  = (360 / 60) * date.getMinutes()
+  const deltaSeconds  = (360 / 60) * date.getSeconds()
+
+  HOURHAND.style.transform    = `rotate(${deltaHours}deg)`
+  MINUTEHAND.style.transform  = `rotate(${deltaMinutes}deg)`
+  SECONDHAND.style.transform  = `rotate(${deltaSeconds}deg)`
+}
+
+setInterval(tick, 1000)
+
+// execute the tick function on page load unless we want to wait for setInterval to kick in (1000ms!!!)
+tick()
